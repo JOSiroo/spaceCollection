154db3156e42a44861c3fd46ae8ac988cc278e3a
ezen <ezen@DESKTOP-O03MT84>
2023-07-28 17:29:21 +0900

    firstCommit

---
+++ spaceCollection/src/test/java/com/sc/spaceCollection/SpaceCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc.spaceCollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
