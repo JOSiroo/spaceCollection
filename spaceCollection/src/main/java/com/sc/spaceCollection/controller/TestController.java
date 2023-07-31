2660c8dc177328955540eccaf4e51371c97e7cfe
PC <PC@192.168.1.106>
2023-07-31 21:17:11 +0900

    누가 톰캣 함정카드 만들었어

--- spaceCollection/src/main/java/com/sc/spaceCollection/controller/TestController.java
+++ spaceCollection/src/main/java/com/sc/spaceCollection/controller/TestController.java
@@ -1,14 +1,18 @@
 package com.sc.spaceCollection.controller;
 
-import org.springframework.aot.hint.annotation.Reflective;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
+	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 	
-	@RequestMapping("/test")
+	
+	@GetMapping("/")
 	public String test() {
+		logger.info("tqtqtqtqtqtqt");
 		return "test";
 	}
 }
