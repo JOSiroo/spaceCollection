fef366208d46bd70a7a12be725ac83bf03a6c8ce
JOSiroo <vincent787@naver.com>
2023-08-04 21:34:48 +0900

    asd

--- spaceCollection/src/main/webapp/js/spaceDetail.js
+++ spaceCollection/src/main/webapp/js/spaceDetail.js
@@ -1,33 +1,26 @@
+const navTabs = document.getElementsByClassName("nav-item");
+const navItems = document.getElementsByClassName("detail-navTab");
+const stickyTop = document.querySelector(".sticky-top"); // sticky-top 요소 선택
 
-const navTabs = document.getElementsByClassName("nav-item")
-const navItems = document.getElementsByClassName("detail-navTab")
+const getAbsoluteTop = (element) => {
+  const rect = element.getBoundingClientRect();
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return rect.top + scrollTop;
+};
 
-const firstTop = navItems[0].offsetTop
-const secondTop = navItems[1].offsetTop
-const thirdTop = navItems[2].offsetTop
-const fourthTop = navItems[3].offsetTop
-const fifthTop = navItems[4].offsetTop
-const sixthTop = navItems[5].offsetTop
-	
-	
-	
-navTabs[0].onclick = function(){
-	window.scroll({top:firstTop, behavior: 'smooth'})
-	
+const scrollToElementWithStickyTop = (element) => {
+  const targetTop = getAbsoluteTop(element);
+  const stickyTopHeight = stickyTop.offsetHeight; // sticky-top 요소의 높이
+  const scrollY = targetTop - stickyTopHeight; // 추가로 위로 스크롤할 위치
+  
+  // 요소 위치로 스크롤하면서 sticky-top 요소의 높이만큼 더 위로 스크롤
+  window.scroll({ top: scrollY, behavior: 'smooth' });
+};
+
+for (let i = 0; i < navTabs.length; i++) {
+  navTabs[i].addEventListener('click', function() {
+    scrollToElementWithStickyTop(navItems[i]);
+  });
 }
-navTabs[1].onclick = function(){
-	window.scroll({top:secondTop, behavior: 'smooth'})
-}
-navTabs[2].onclick = function(){
-	window.scroll({top:thirdTop, behavior: 'smooth'})
-}
-navTabs[3].onclick = function(){
-	window.scroll({top:fourthTop, behavior: 'smooth'})
-}
-navTabs[4].onclick = function(){
-	window.scroll({top:fifthTop, behavior: 'smooth'})
-}
-navTabs[5].onclick = function(){
-	window.scroll({top:sixthTop, behavior: 'smooth'})
-}
+
 
