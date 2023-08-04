d79abf2c1faf0e52ce2032402b6969f1dae1d8b8
JOSiroo <vincent787@naver.com>
2023-08-04 21:42:00 +0900

    asd

--- spaceCollection/src/main/webapp/js/spaceDetail.js
+++ spaceCollection/src/main/webapp/js/spaceDetail.js
@@ -1,22 +1,14 @@
 const navTabs = document.getElementsByClassName("nav-item");
 const navItems = document.getElementsByClassName("detail-navTab");
-<<<<<<< HEAD
 const stickyTop = document.querySelector(".sticky-top"); // sticky-top 요소 선택
-=======
->>>>>>> branch 'main' of https://github.com/JOSiroo/spaceCollection.git
-
-<<<<<<< HEAD
 const getAbsoluteTop = (element) => {
   const rect = element.getBoundingClientRect();
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   return rect.top + scrollTop;
 };
-=======
 // navItems의 offsetTop 값에 추가할 값을 설정합니다.
 const offsetValue = 60;
->>>>>>> branch 'main' of https://github.com/JOSiroo/spaceCollection.git
 
-<<<<<<< HEAD
 const scrollToElementWithStickyTop = (element) => {
   const targetTop = getAbsoluteTop(element);
   const stickyTopHeight = stickyTop.offsetHeight; // sticky-top 요소의 높이
@@ -33,7 +25,6 @@ for (let i = 0; i < navTabs.length; i++) {
 }
 
 
-=======
 // 각 navTabs 요소에 클릭 이벤트를 추가합니다.
 for (let i = 0; i < navTabs.length; i++) {
     navTabs[i].onclick = function () {
@@ -42,4 +33,3 @@ for (let i = 0; i < navTabs.length; i++) {
     };
 }
 	
->>>>>>> branch 'main' of https://github.com/JOSiroo/spaceCollection.git
