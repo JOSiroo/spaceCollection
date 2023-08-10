518c240bef3f88132a6602b11eb129e70938a776
ezen <ezen@DESKTOP-O03MT84>
2023-08-10 15:04:38 +0900

    123

--- spaceCollection/src/main/java/com/sc/spaceCollection/board/model/BoardDAO.java
+++ spaceCollection/src/main/java/com/sc/spaceCollection/board/model/BoardDAO.java
@@ -1,10 +1,15 @@
 package com.sc.spaceCollection.board.model;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
+
+import com.sc.spaceCollecion.common.SearchVO;
 
 @Mapper
 public interface BoardDAO {
 	List<BoardVO> selectByBoardTypeId(String boardTypeName);
+	List<Map<String, Object>> selectBoardAll(SearchVO searchVo);
+	int getTotalRecord(SearchVO searchVo);
 }
