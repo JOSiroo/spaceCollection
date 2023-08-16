8cd5c78ca132e36ec6528ae8c32680ee02859c3b
JOSiroo <vincent787@naver.com>
2023-08-16 15:14:31 +0900

    푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하푸하하하

--- spaceCollection/src/main/java/com/sc/spaceCollection/reservation/model/ReservationDAO.java
+++ spaceCollection/src/main/java/com/sc/spaceCollection/reservation/model/ReservationDAO.java
@@ -1,10 +1,12 @@
 package com.sc.spaceCollection.reservation.model;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ReservationDAO {
 	int insertReservation(ReservationVO vo);
 	ReservationVO selectReservationByDayAndNum(ReservationVO vo);
-	ReservationVO showReservation(int reservationNum);
+	Map<String, Object> showReservation(int reservationNum);
 }
