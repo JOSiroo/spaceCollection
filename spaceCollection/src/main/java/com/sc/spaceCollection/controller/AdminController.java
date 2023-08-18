f1db2459b9e64843c83bcca7ef38f0e877fd9ab0
user <user@192.168.1.101>
2023-08-18 20:29:56 +0900

    파브레가스요

--- spaceCollection/src/main/java/com/sc/spaceCollection/controller/AdminController.java
+++ spaceCollection/src/main/java/com/sc/spaceCollection/controller/AdminController.java
@@ -1,8 +1,15 @@
 package com.sc.spaceCollection.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -12,7 +19,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.google.gson.JsonObject;
 import com.sc.spaceCollection.admin.model.AdminService;
 import com.sc.spaceCollection.board.model.BoardListVO;
 import com.sc.spaceCollection.board.model.BoardService;
@@ -29,7 +40,6 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -335,7 +345,71 @@ public class AdminController {
 		model.addAttribute("url", url);
 		
 		return "admin/common/message";
-		
-		
+	}
+	
+	@PostMapping("/board/fileUpload")
+	@ResponseBody
+	public String fileUpload(HttpServletRequest request, HttpServletResponse response,
+			MultipartHttpServletRequest multiFile) throws IOException {
+		//Json 객체 생성
+				JsonObject json = new JsonObject();
+				// Json 객체를 출력하기 위해 PrintWriter 생성
+				PrintWriter printWriter = null;
+				OutputStream out = null;
+				//파일을 가져오기 위해 MultipartHttpServletRequest 의 getFile 메서드 사용
+				MultipartFile file = multiFile.getFile("upload");
+				//파일이 비어있지 않고(비어 있다면 null 반환)
+				if (file != null) {
+					// 파일 사이즈가 0보다 크고, 파일이름이 공백이 아닐때
+					if (file.getSize() > 0 && StringUtils.isNotBlank(file.getName())) {
+						if (file.getContentType().toLowerCase().startsWith("image/")) {
+
+							try {
+								//파일 이름 설정
+								String fileName = file.getName();
+								//바이트 타입설정
+								byte[] bytes;
+								//파일을 바이트 타입으로 변경
+								bytes = file.getBytes();
+								//파일이 실제로 저장되는 경로 
+								String uploadPath = request.getServletContext().getRealPath("/board_images/");
+								//저장되는 파일에 경로 설정
+								File uploadFile = new File(uploadPath);
+								if (!uploadFile.exists()) {
+									uploadFile.mkdirs();
+								}
+								//파일이름을 랜덤하게 생성
+								fileName = UUID.randomUUID().toString();
+								//업로드 경로 + 파일이름을 줘서  데이터를 서버에 전송
+								uploadPath = uploadPath + "/" + fileName;
+								out = new FileOutputStream(new File(uploadPath));
+								out.write(bytes);
+								
+								//클라이언트에 이벤트 추가
+								printWriter = response.getWriter();
+								response.setContentType("text/html");
+								
+								//파일이 연결되는 Url 주소 설정
+								String fileUrl = request.getContextPath() + "/board_images/" + fileName;
+								
+								//생성된 jason 객체를 이용해 파일 업로드 + 이름 + 주소를 CkEditor에 전송
+								json.addProperty("uploaded", 1);
+								json.addProperty("fileName", fileName);
+								json.addProperty("url", fileUrl);
+								printWriter.println(json);
+							} catch (IOException e) {
+								e.printStackTrace();
+							} finally {
+								if(out !=null) {
+									out.close();
+								}
+								if(printWriter != null) {
+									printWriter.close();
+								}
+							}
+						}
+					}
+				}
+					return null;
 	}
 }
