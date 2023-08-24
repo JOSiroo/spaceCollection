c2c6f6402902b830e94db1ca44526ce8918906b5
eunjung <135147602+eunjung15@users.noreply.github.com>
2023-08-25 02:32:01 +0900

    캡차

--- spaceCollection/src/main/java/com/sc/spaceCollection/usermain/model/ApiExamCaptchaNkeyResult.java
+++ spaceCollection/src/main/java/com/sc/spaceCollection/usermain/model/ApiExamCaptchaNkeyResult.java
@@ -13,8 +13,8 @@ import java.util.Map;
 public class ApiExamCaptchaNkeyResult {
 
     public static void main(String[] args) {
-        String clientId = "YOUR_CLIENT_ID";//애플리케이션 클라이언트 아이디값";
-        String clientSecret = "YOUR_CLIENT_SECRET";//애플리케이션 클라이언트 시크릿값";
+        String clientId = "_w2MbHAObKkDYPDgmbHR";//애플리케이션 클라이언트 아이디값";
+        String clientSecret = "spVJnrMWRL";//애플리케이션 클라이언트 시크릿값";
 
         String code = "1"; // 키 발급시 0,  캡차 이미지 비교시 1로 세팅
         String key = "YOUR_CAPTCHA_KEY"; // 캡차 키 발급시 받은 키값
