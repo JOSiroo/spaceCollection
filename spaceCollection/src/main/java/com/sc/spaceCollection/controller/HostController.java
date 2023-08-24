19d4a848940f53dc2c0cf2fd9852f192243e3cc4
Yeerinn <flxmfemfor@naver.com>
2023-08-24 17:45:27 +0900

    0824 수업 끝

--- spaceCollection/src/main/java/com/sc/spaceCollection/controller/HostController.java
+++ spaceCollection/src/main/java/com/sc/spaceCollection/controller/HostController.java
@@ -1,9 +1,7 @@
 package com.sc.spaceCollection.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sc.spaceCollection.host.model.HostService;
+import com.sc.spaceCollection.host.model.SpaceCategoryAllVO;
 import com.sc.spaceCollection.host.model.SpaceTypeVO;
 
 import lombok.RequiredArgsConstructor;
@@ -32,38 +31,20 @@ public class HostController {
 		return "host/index";
 	}
 	
-	@GetMapping("/registration")
+	@GetMapping("/registration1")
 	public String registration(Model model) {
 		//1
 		logger.info("공간 등록 첫페이지 보여주기");
 		
 		//2
-		List<List<Map<String, Object>>> type = new ArrayList<>();
-		List<SpaceTypeVO> map = new ArrayList<>();
-		List<SpaceTypeVO> category = hostService.selectSpaceCategory();
-		logger.info("카테고리 ={}",category);
-		
-		for (int i = 0; i < category.size(); i++) {
-			map.add(category.get(i));
-			logger.info("맵 ={}",map);
-			
-			String categoryName = map.get(i).getCategoryName();
-			logger.info("카테고리번호 = {}", categoryName);
-			
-			List<Map<String, Object>> space = hostService.selectSpaceType(categoryName);
-			logger.info("스페이스 = {}", space);
-			type.add(space); 
-			logger.info("공간타입 카테고리번호로 조회, type.size = {}", type.size());
-			
-		}
-		logger.info("타입 = {}", type);
+		List<SpaceCategoryAllVO> type = hostService.selectSpaceCategory();
+		logger.info("type = {}", type);
 
 		//3
 		model.addAttribute("type", type);
-		model.addAttribute("map2", map);
 		
 		//4
-		return "host/registration";
+		return "host/registration1";
 	}
 	
 	@RequestMapping("/report/account")
--- spaceCollection/src/main/java/com/sc/spaceCollection/host/model/HostService.java
+++ spaceCollection/src/main/java/com/sc/spaceCollection/host/model/HostService.java
@@ -4,6 +4,6 @@ import java.util.List;
 import java.util.Map;
 
 public interface HostService {
-	List<SpaceTypeVO> selectSpaceCategory();
-	List<Map<String, Object>> selectSpaceType(String categoryName);
+	List<SpaceTypeVO> selectSpaceType(int categoryNo);
+	List<SpaceCategoryAllVO> selectSpaceCategory();
 }
