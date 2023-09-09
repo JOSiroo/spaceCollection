bef93d97527f5ea52601ba0f89a38aca2e96d14b
JOSiroo <vincent787@naver.com>
2023-09-09 16:21:23 +0900

    기획전 뷰페이지

--- spaceCollection/src/main/java/com/sc/spaceCollection/collection/model/CollectionDAO.java
+++ spaceCollection/src/main/java/com/sc/spaceCollection/collection/model/CollectionDAO.java
@@ -4,9 +4,10 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface CollectionDAO {
-	List<Map<String, Object>> showCollectionList(int scNum);
+	List<Map<String, Object>> showCollectionList(@Param("scCode")int scCode);
 	String selectCollectionName(int scNum);
 }
